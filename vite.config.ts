3e3e4d204120f1fc2b08f0e21a0dbd010e41ccd4
zahrazn21 <zahrazn2024@gmail.com>
2025-03-01 12:29:39 +0330

    cahflow pwa

--- vite.config.ts
+++ vite.config.ts
@@ -51,12 +51,12 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: "/icons/logo192.png",
+            src: "logo192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "/icons/logo512.png",
+            src: "logo512.png",
             sizes: "512x512",
             type: "image/png"
           }
